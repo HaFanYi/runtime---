26b83c7eb8a383a92a1212c9175ca030d11ddee0
watchmanLee <woxinyiran19910912@163.com>
2017-03-14 23:37:52 +0800

    Add files via upload

---
+++ runtime--归档解档Tests/runtime______Tests.m
@@ -0,0 +1,39 @@
+//
+//  runtime______Tests.m
+//  runtime--归档解档Tests
+//
+//  Created by Hervey on 2017/3/14.
+//  Copyright © 2017年 Hervey. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface runtime______Tests : XCTestCase
+
+@end
+
+@implementation runtime______Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
